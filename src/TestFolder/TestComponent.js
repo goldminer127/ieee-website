108376000cce4dcc8115bbf820b231d56828c4bb
Joshua Angulo <jangulo@albany.edu>
2024-02-12 01:07:57 -0500

    IEEE/Test2 or smth I forgot the name of the previous one

--- src/TestFolder/TestComponent.js
+++ src/TestFolder/TestComponent.js
@@ -5,13 +5,9 @@ const TestComponent = () =>
 {
     //Replace SampleComponent with your component to test inside the div
     //You can also change the style of this div if needed
-    // RAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
     return(
         <Box component = 'div' sx={{width: '100vw', height: '100vh'}}>
             <SampleComponent text = "Let's get started"/>
-            <Box>
-            <SampleComponent text = "RAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"/>
-            </Box>
         </Box>
     );
 }
