f9f8a6a804a7195adab14b5fc64fd6652aab5aef
Johannson <johannsonramos@gmail.com>
2024-02-28 00:52:06 -0500

    Footer Second Version

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -11,12 +11,14 @@ const Footer =()=>{
       <div id = "footer">
         <div id = "footer-logo">
          <img src ={FooterLogo}  className = "footer-ieeeIcon" alt="IEEE"/>
+         <span className = "logo-text"> AT UALBANY </span>
          </div>
            <div className = "footer-socialIcons">
+           <img src ={InstagramLogo}  className = "footer-socialIcon" alt="Instagram"/>
+           <img src ={LinkedInLogo}  className = "footer-socialIcon" alt="LinkedIn"/>
             <img src ={YoutubeLogo}  className = "footer-socialIcon" alt="YouTube"/>
             <img src ={DiscordLogo}  className = "footer-socialIcon" alt="Discord"/>
-            <img src ={LinkedInLogo}  className = "footer-socialIcon" alt="LinkedIn"/>
-            <img src ={InstagramLogo}  className = "footer-socialIcon" alt="Instagram"/>
+            
         </div>
       </div>
         
